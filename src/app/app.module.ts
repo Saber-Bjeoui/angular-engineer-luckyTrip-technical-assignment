8bd4faf52e51b1966e3a7a3abe1848ca018c8a0d
Saber Bjeoui <bjeoui.saber@gmail.com>
2022-04-17 23:51:29 +0100

    Generate files for SearchPageComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchInputComponent } from './search-input/search-input.component';
 import { HeaderComponent } from './header/header.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchInputComponent,
-    HeaderComponent
+    HeaderComponent,
+    SearchPageComponent
   ],
   imports: [
     BrowserModule,
